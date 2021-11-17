6e119c3bb8bdc2ff189cc2a6b7e35279d19c1dd5
Steffania Salgado <86125323+SalgadoSteffania@users.noreply.github.com>
2021-11-17 15:43:04 -0600

    Iniciando

---
+++ Datos/Entidades/Producto.cs
@@ -0,0 +1,21 @@
+﻿using Datos.Enum;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Datos.Entidades
+{
+    public class Producto
+    {
+
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public int Existencia { get; set; }
+        public decimal Precio { get; set; }
+        public DateTime FechaIngreso { get; set; }
+        public Modelos Modelos { get; set; }
+
+
+    }
+}
---
+++ Datos/Enum/modelos.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Datos.Enum
+{
+   public enum Modelos
+    {
+
+      Unidades,
+      Litros,
+      Mililitros,
+      Libras,
+      Onzas,
+      Kilogramos
+
+    }
+}
